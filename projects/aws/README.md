c964e6492965f02dafeea2f53e3b21ccde28a126
Brian Claridge <brian.claridge@workiva.com>
2015-11-05 15:30:06 -0700

    more random/template options

--- projects/aws/README.md
+++ projects/aws/README.md
@@ -5,17 +5,17 @@
 ### cli
 
 ```Shell
-$ docker-compose build base
-$ docker-compose run config
-$ docker-compose run cli kinesis list-streams
-$ docker-compose run cli kinesis put-records --generate-cli-skeleton
+docker-compose build base
+docker-compose run config
+docker-compose run cli kinesis list-streams
+docker-compose run cli kinesis put-records --generate-cli-skeleton
 ```
 
 ### kinesis
 
 ```Shell
-$ docker-compose build base
-$ docker-compose run config
-$ docker-compose run kinesis list
-$ docker-compose run kinesis feed {stream-name} 15
+docker-compose build base
+docker-compose run config
+docker-compose run kinesis list
+docker-compose run kinesis feed {stream-name} 15
 ```
--- projects/aws/kinesis/src/aws-kinesis/random.go
+++ projects/aws/kinesis/src/aws-kinesis/random.go
@@ -10,16 +10,30 @@ import (
 )
 
 type RandomData struct {
-	Index     int
-	SillyName string
-	EpochTime int64
+	Index              int
+	Noun               string
+	Adjective          string
+	SillyName          string
+	NumberOneToHundred int
+	FloatZeroToHundred float64
+	Paragraph          string
+	Boolean            bool
+	EpochTime          int64
+	IpV4Address        string
 }
 
 func NewRandomData(index int) *RandomData {
 	return &RandomData{
-		Index:     index,
-		SillyName: randomdata.SillyName(),
-		EpochTime: time.Now().UnixNano() / 1000000,
+		Index:              index,
+		Noun:               randomdata.Noun(),
+		Adjective:          randomdata.Adjective(),
+		SillyName:          randomdata.SillyName(),
+		NumberOneToHundred: randomdata.Number(0, 100),
+		FloatZeroToHundred: randomdata.Decimal(0, 100),
+		Paragraph:          randomdata.Paragraph(),
+		Boolean:            randomdata.Boolean(),
+		EpochTime:          time.Now().UnixNano() / 1000000,
+		IpV4Address:        randomdata.IpV4Address(),
 	}
 }
 
